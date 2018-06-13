3b3ecf2694094288685f553ea109a6b62721bf2e
Stiegnate <28430066+Stiegnate@users.noreply.github.com>
2018-06-14 00:09:45 +0800

    add pseudo code for Action

--- App/Action/pseudo_example.py
+++ App/Action/pseudo_example.py
@@ -1,21 +1,16 @@
-# Coding Style
-
-
-# Windows
-### pseudo code
 def left_click(t=0.1):
-    ''' 
+    '''
     t: the delay time between pressing left click and releasing left click
-    ''' 
+    '''
     press()
     time_delay()
     release()
 
 def right_click(t=0.1):
-    as left_click 
+    as left_click
 
 def move(pos1, pos2):
-    ''' 
+    '''
     pos1: initial position of mouse [x1,y1]
     pos2: final position of mouse [x2,y2]
     '''
@@ -34,12 +29,8 @@ def scroll_click():
 def keyboard_type('string',t=0.1):
     '''
     example:
-	input:
+        input:
             keyboard_type('my name')
         output:
             typing the keyboard 'm', 'y', ... and so on with time delay t = 0.1(sec).
-    '''    
-
-# macOS
-
-# Ubuntu
+    '''
